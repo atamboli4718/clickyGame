e50d4d12425cc9df0405b6625a119e424bbf2144
Alicia Tamboli <atamboli4718@gmail.com>
2019-09-02 17:21:35 -0400

    added counters and random number generators

--- clickygame/src/components/ResultList/ResultList.js
+++ clickygame/src/components/ResultList/ResultList.js
@@ -6,12 +6,35 @@ function ResultList(props) {
   return (
     <Row>
       {props.results.map(result => (
-        <Col xs='3' className="sizing" key={result.id}>
-          <img alt={result.title} className="img-fluid" src={result.images.original_still.url} />
-        </Col>
+        <ResultItem key={result.id} result={result} />
       ))}
     </Row>
   );
+}
+
+class ResultItem extends React.Component {
+  state = {
+    counter: 0,
+    still: true,
+    value: Math.floor(Math.random() * 12) + 1,
+  };
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+      still: !this.state.still,
+    });
+    console.log(this.state.value)
+  }
+
+  render() {
+    return(
+      <Col xs='3' className="sizing" onClick={this.handleClick}>
+        <p>Click Counter: </p>{this.state.counter}
+        <img alt={this.props.result.title} className="img-fluid" src={this.state.still ? this.props.result.images.original_still.url : this.props.result.images.original.url} />
+      </Col>
+    );
+  }
 }
 
 export default ResultList;
--- clickygame/src/components/WinCoutner/WinCounter.js
+++ clickygame/src/components/WinCoutner/WinCounter.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./style.css";
 
-function WinCounter(props) {
-    return (
-        <div>
-            <p className="wins">Wins: {props.wins} </p>
-            <p className="losses">Losses: {props.losses}</p>
-        </div>
-    )
+class WinCounter extends React.Component {
+    targetNumber= (Math.floor(Math.random() * 104) + 50);
+
+    render() {
+        return (
+            <div>
+                <p className="targetNumber">Targer Number: {this.targetNumber}</p>
+                <p className="wins">Wins: {this.props.wins} </p>
+                <p className="losses">Losses: {this.props.losses}</p>
+                <p className="yourGuess">Your Guess: {this.props.guess}</p>
+            </div>
+        );
+    }
 }
+
+
 
 export default WinCounter;
