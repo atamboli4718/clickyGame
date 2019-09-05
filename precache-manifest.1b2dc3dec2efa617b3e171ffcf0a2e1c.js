89a2fa2a528b236e55e94509bce6d0020d2e08ec
Alicia Tamboli <atamboli4718@gmail.com>
2019-09-05 19:46:11 -0400

    Updates

---
+++ precache-manifest.1b2dc3dec2efa617b3e171ffcf0a2e1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8748d7dc76685a1d46574968dcadcbc0",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "1165b87235511e4a173d",
+    "url": "/clickyGame/static/css/main.673a606e.chunk.css"
+  },
+  {
+    "revision": "1c92a245ac994affd03c",
+    "url": "/clickyGame/static/js/2.41a003b3.chunk.js"
+  },
+  {
+    "revision": "1165b87235511e4a173d",
+    "url": "/clickyGame/static/js/main.cb6c91cf.chunk.js"
+  },
+  {
+    "revision": "b04e682f02a4fb6b28c9",
+    "url": "/clickyGame/static/js/runtime~main.f5135437.js"
+  }
+]);
