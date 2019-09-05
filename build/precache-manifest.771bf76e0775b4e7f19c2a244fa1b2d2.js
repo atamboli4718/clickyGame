82137edbbfd587a83512298e546f228b59e896ce
Alicia Tamboli <atamboli4718@gmail.com>
2019-09-05 19:41:25 -0400

    adding changes

---
+++ build/precache-manifest.771bf76e0775b4e7f19c2a244fa1b2d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8ab93ee2d8420582ebc79b04711138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191cab06ba9bb25e8e96",
+    "url": "/static/css/main.673a606e.chunk.css"
+  },
+  {
+    "revision": "1c92a245ac994affd03c",
+    "url": "/static/js/2.41a003b3.chunk.js"
+  },
+  {
+    "revision": "191cab06ba9bb25e8e96",
+    "url": "/static/js/main.b5328fe2.chunk.js"
+  },
+  {
+    "revision": "772f81129d157a671b37",
+    "url": "/static/js/runtime~main.d0a26a9b.js"
+  }
+]);
